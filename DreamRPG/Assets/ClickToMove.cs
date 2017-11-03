dd4d71c45a15542c9e0ef26841a157122bef9bd0
spartann123 <robertojr_hofmann@hotmail.com>
2017-11-03 17:16:42 -0200

    Movimentacao
    Ajustes na movimentacao

--- DreamRPG/Assets/ClickToMove.cs
+++ DreamRPG/Assets/ClickToMove.cs
@@ -7,23 +7,24 @@ public class ClickToMove : MonoBehaviour {
     private Vector3 position;
     public UnityEngine.AI.NavMeshAgent navAgent;
 	public SpriteRenderer sprite;
-	float time;
+	public float time = 0.5f;
 	bool refreshTime;
+	public Vector3 distanceFromPoint;
    // public CharacterController controller;
 	// Use this for initialization
 	void Start () {
     //    position = transform.position;
         navAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-		time = Time.deltaTime;
 		refreshTime = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (refreshTime) {
-			time = Time.deltaTime;
+			time = 0.4f;
 			refreshTime = false;
 		}
+		time -= Time.deltaTime;
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
@@ -32,29 +33,25 @@ public class ClickToMove : MonoBehaviour {
             if (Physics.Raycast(ray, out hit, 1000))
             {
             //    position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
-                navAgent.SetDestination(hit.point);
-				if (time * 1000 <= 1) {
-					sprite.flipX = true;
-
-				} else if(time*1000 >= 2) {
-					sprite.flipX = false;
-					refreshTime = true;
-				}
+				navAgent.SetDestination(hit.point);
+				distanceFromPoint = hit.point;
             }
         }
+		if ((time <= 0.0f) && Vector3.Distance(navAgent.transform.position, distanceFromPoint) > 1.5) {
+			sprite.flipX = !sprite.flipX;
+			refreshTime = true;
+		}
+
         moveToPosition();    
 	}
     void moveToPosition() { 
     
    //     if(Vector3.Distance(transform.position, position) > 1){
             Quaternion newRotation = Quaternion.LookRotation(position - transform.position);
-            Debug.Log("rotation x: " + newRotation.x);
-            Debug.Log("rotation Y: " + newRotation.y);
-            Debug.Log("rotation z: " + newRotation.z);
             newRotation.x = 0f;
             newRotation.z = 0f;
             newRotation.y = 0f;
-            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 50);
          //   controller.SimpleMove(transform.forward * speed);
      //   }
     }
